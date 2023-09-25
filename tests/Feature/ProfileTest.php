8b3ce9a203f683b1d2241694e2b9cd57376188c5
IvSDF <demchyshyn.ivan@gmail.com>
2023-09-25 18:10:07 +0300

    create_managing_users_policy

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    public function test_user_can_delete_their_account(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->delete('/profile', [
+                'password' => 'password',
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/');
+
+        $this->assertGuest();
+        $this->assertSoftDeleted($user->fresh());
+    }
+}
