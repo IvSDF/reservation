3a00aaf4d75ec93fdb28f0e1eb829c5dd70c94e3
IvSDF <demchyshyn.ivan@gmail.com>
2023-09-27 14:01:23 +0300

    create_manage_activities and_assign_guides

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Activity;
 use App\Models\Company;
+use App\Policies\CompanyActivityPolicy;
 use App\Policies\CompanyUserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Company::class => CompanyUserPolicy::class
+        Company::class => CompanyUserPolicy::class,
+        Activity::class => CompanyActivityPolicy::class,
     ];
 
     /**
