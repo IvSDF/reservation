f43ea74e30004fdfca9f2f81bb99c901b033774f
IvSDF <demchyshyn.ivan@gmail.com>
2023-09-28 15:57:14 +0300

    create_guide_export_PDF

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'role_id' => Role::CUSTOMER->value,
+            'role_id' => Role::CUSTOMER,
         ];
     }
 
@@ -42,6 +42,13 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'role_id' => Role::ADMINISTRATOR->value,
+        ]);
+    }
+
+    public function companyOwner(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role_id' => Role::COMPANY_OWNER->value,
         ]);
     }
 
