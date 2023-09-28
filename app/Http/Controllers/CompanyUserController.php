2df172fbf1a79fc118d48e833290567ec3a85a39
IvSDF <demchyshyn.ivan@gmail.com>
2023-09-28 13:12:01 +0300

    create_sending_invitation_mail

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers;
 use App\Enums\Role;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Mail\UserRegistrationInvite;
 use App\Models\Company;
 use App\Models\User;
+use App\Models\UserInvitation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class CompanyUserController extends Controller
 {
@@ -31,12 +35,14 @@ class CompanyUserController extends Controller
     {
         $this->authorize('create', $company);
 
-        $company->users()->create([
-            'name' => $request->input('name'),
+        $invitation = UserInvitation::create([
             'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
+            'token' => Str::uuid(),
+            'company_id' => $company->id,
             'role_id' => Role::COMPANY_OWNER->value,
         ]);
+
+        Mail::to($request->input('email'))->send(new UserRegistrationInvite($invitation));
 
         return to_route('companies.users.index', $company);
     }
